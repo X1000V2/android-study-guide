2e0bcd34ec8d0cd3e15c90907ec37e14bf849d45
x1000z500 <lbarrosov@gmail.com>
2019-08-10 12:03:13 +0200

    Added screen 1 in navigation component.

---
+++ app/src/main/java/luis/barroso/androidstudyguide/NavigationScreen1Fragment.kt
@@ -0,0 +1,17 @@
+package luis.barroso.androidstudyguide
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class NavigationScreen1Fragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navigation_screen1, container, false)
+    }
+}

b0de48eb660bdc941242b45c3725bd2bbbf90516
x1000z500 <lbarrosov@gmail.com>
2019-08-13 22:03:59 +0200

    Created screen to test job scheduler in Android Core.

--- app/src/main/java/luis/barroso/androidstudyguide/AndroidCoreScreenActivity.kt
+++ app/src/main/java/luis/barroso/androidstudyguide/AndroidCoreScreenActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_android_core_screen.*
 import luis.barroso.androidstudyguide.androidCore.*
+import luis.barroso.androidstudyguide.androidCore.jetpack.jobScheduler.AndroidCoreJobSchedulerActivity
 
 class AndroidCoreScreenActivity : AppCompatActivity() {
 
@@ -17,6 +18,7 @@ class AndroidCoreScreenActivity : AppCompatActivity() {
         button_localization.setOnClickListener { goToLocalizationScreen() }
         button_notifications.setOnClickListener { goToNotificationsScreen() }
         button_jetpack.setOnClickListener { goToJetpackScreen() }
+        button_job_scheduler.setOnClickListener { goToJobSchedulerScreen() }
     }
 
     private fun goToToastScreen() = startActivity(Intent(this, AndroidCoreToastsScreenActivity::class.java))
@@ -28,4 +30,6 @@ class AndroidCoreScreenActivity : AppCompatActivity() {
     private fun goToNotificationsScreen() = startActivity(Intent(this, AndroidCoreNotificationsScreenActivity::class.java))
 
     private fun goToJetpackScreen() = startActivity(Intent(this, AndroidCoreJetpackScreenActivity::class.java))
+
+    private fun goToJobSchedulerScreen() = startActivity(Intent(this, AndroidCoreJobSchedulerActivity::class.java))
 }
---
+++ app/src/main/java/luis/barroso/androidstudyguide/androidCore/jetpack/jobScheduler/AndroidCoreJobSchedulerActivity.kt
@@ -0,0 +1,13 @@
+package luis.barroso.androidstudyguide.androidCore.jetpack.jobScheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import luis.barroso.androidstudyguide.R
+
+class AndroidCoreJobSchedulerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_android_core_job_scheduler)
+    }
+}
